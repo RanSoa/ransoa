b97b7c93f3ef8722f8c0cc9d738449b263b61c8c
RanmaDev <mohamedmario153@gmail.com>
2024-03-27 19:52:45 -0700

    changes

---
+++ main.js
@@ -0,0 +1,45 @@
+
+//header
+
+gsap.from('.logo', {opacity: 0,  duration: 1, delay: 2, y:10});
+gsap.from('.navigation .ww', {opacity: 0,  duration: 1, delay: 2.2, y:30, stagger: 0.2});
+gsap.from('.navigation .zz', {opacity: 0,  duration: 1, delay: 2.4, y:30, stagger: 0.2});
+gsap.from('.main h2', {opacity: 0,  duration: 1, delay: 1.6, y:30});
+gsap.from('.main span', {opacity: 0,  duration: 1, delay: 1.8, y:30});
+gsap.from('.main h3', {opacity: 0,  duration: 1, delay: 2.2, y:30});
+gsap.from('.main .main-btn', {opacity: 0,  duration: 1, delay: 2.1, y:30});
+gsap.from('.social-icons', {opacity: 0,  duration: 1, delay: 2.3, y:30});
+
+//services
+
+gsap.from('.Cards .title', {opacity: 0,  duration: 1, delay: 2.3, y:30});
+gsap.from('.Cards .content', {opacity: 0,  duration: 1, delay: 2.5, y:30});
+
+
+gsap.from('.Contact .title', {opacity: 0,  duration: 1, delay: 2.5, y:30});
+gsap.from('.Contact .telegram_card', {opacity: 0,  duration: 1, delay: 2.5, x:-30});
+gsap.from('.Contact .discord_card', {opacity: 0,  duration: 1, delay: 2.5, x:30});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
